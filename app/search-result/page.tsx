d13991dec6c625112cefe95b51b14f370ef24fc9
sakib-diu <251-50-006@diu.edu.bd>
2025-07-04 21:55:38 +0600

    #8 fix vercel errors 2

--- app/search-result/page.tsx
+++ app/search-result/page.tsx
@@ -1,10 +1,15 @@
 import { getAllProductsPaginated } from '@/actions/products-actions';
 import SearchResult from '@/components/search-result/searchResult';
+import { Suspense } from 'react';
 
 const SearchResultServerComponent = async () => {
-    const { products: productData } = await getAllProductsPaginated();
+  const { products: productData } = await getAllProductsPaginated();
 
-    return <SearchResult productData={productData} />
+  return (
+    <Suspense fallback={<></>}>
+      <SearchResult productData={productData} />
+    </Suspense>
+  )
 };
 
 export default SearchResultServerComponent;
