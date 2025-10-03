efb7c7d12f23301418264e4c455e19cd60246dd3
sakibullah2006 <sakibullah.dev@gmail.com>
2025-10-04 00:24:29 +0600

    fix: Update DashboardSidebar to use usePathname and useSearchParams for improved URL handling and set scroll to false

--- components/Dashboard/DashboardSidebar.tsx
+++ components/Dashboard/DashboardSidebar.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useAuth } from '@/context/AuthContext'
 import { Customer } from '@/types/customer-type'
 import * as Icon from "@phosphor-icons/react/dist/ssr"
@@ -17,12 +17,14 @@ interface DashboardSidebarProps {
 const DashboardSidebar = ({ customer, activeTab, setActiveTab }: DashboardSidebarProps) => {
     const { logout } = useAuth()
     const router = useRouter()
+    const pathName = usePathname()
+    const searchParams = useSearchParams()
 
     useEffect(() => {
-        const params = new URLSearchParams(window.location.search)
+        const params = new URLSearchParams(searchParams.toString())
         params.set('tab', activeTab)
-        const newUrl = `${window.location.pathname}?${params.toString()}`
-        router.replace(newUrl)
+        const newUrl = `${pathName}?${params.toString()}`
+        router.replace(newUrl, { scroll: false })
     }, [activeTab, router])
 
     const handleLogout = () => {
